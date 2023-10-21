134ccff608f080976f47237c8ab7b6688fbbb03e
KadarM <101024695+K-Marcell@users.noreply.github.com>
2023-10-21 22:18:37 +0200

    Backend logic implementation
    Main backend logic implemented:
    /index -> main site
    /createstop/:stopname -> create a stop | ID is automatically incremented and should be viewed with a database viewer(sqlite -> sqlite.exe which can be downloaded on sqlite's website.) or a stop viewer hidden link can be implemented -> not recommended for security reasons.
    /addtrip/:startplace/:starttime/:estimatedarrival -> add a trip which has a starting place(starting place given in name form(automatically converted to id equivalent)), start time and estimated arrival(which has their form as "12:34".
    /addstop/:tourid/:stopid -> add a stop to a tour/trip, this stop being added to the end of the trip. 1 -> 2 -> 3(new)
    /removestop/:tourid/:stopid -> remove a stop from a specific tour. stop can be in any place: 1 -> 2(to remove) -> 3
    /fullremovestop/:stopid -> remove a stop completely | THIS WILL CAUSE EACH
    /removetrip/:tourid -> remove a tour/trip with a specific id, this removing everything related to that tour/trip.

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const sqlite = require("sqlite3").verbose();
+const app = express();
+
+let db = new sqlite.Database("db/menetrend.db", (err) => {
+    if (err)
+        return console.log(err.message);
+    console.log("Connected to webserver database!");
+});
+app.listen(3000, () => console.log("Server running on port 3000!"));
+
+app.get("/index", function(req, res) {
+    res.render("../html/index.ejs", { 
+    });
+});
+app.get('/createstop/:stopname', function(req, res) {
+    const stopname = req.params.stopname;
+    db.run("INSERT INTO allomasok(allomasNev) VALUES(?);", [stopname], function(err) {
+        if(err)
+            return console.log(err);
+    });
+    res.end(`Allomas hozzaadva: ${req.params.stopname}`);
+  });
+
+
+app.get('/addtrip/:startplace/:starttime/:estimatedarrival', function(req, res) { //starttime: 00:00 || estimatedarrival: 00:00
+    const starthour = req.params.starttime.split(":")[0];
+    const startminute = req.params.starttime.split(":")[1];
+    const etahour = req.params.estimatedarrival.split(":")[0];
+    const etaminute = req.params.estimatedarrival.split(":")[1];
+    const startplace = req.params.startplace;
+    let starttime = new Date();
+    starttime.setHours(parseInt(starthour)+2);
+    starttime.setMinutes(parseInt(startminute));
+    let etatime = new Date();
+    etatime.setHours(parseInt(etahour)+2);
+    etatime.setMinutes(parseInt(etaminute));
+    db.run("INSERT INTO menetrend(jaratKezdes, jaratKezd, jaratVeg) VALUES(?, ?, ?)", [startplace, starttime.toISOString().slice(0, 19).replace('T', ' '), etatime.toISOString().slice(0, 19).replace('T', ' ')], function(err) {
+        if(err)
+            return console.log(err);
+    });
+    db.run("INSERT INTO jaratadat(allomasok) VALUES((SELECT allomasId FROM allomasok WHERE allomasNev = ?))", [startplace], function(err) {
+        if(err)
+            db.run("INSERT INTO allomasok(allomasNev) VALUES(?);", [startplace], function(err) {
+                if(err)
+                    return console.log(err);
+            });
+            db.run("INSERT INTO jaratadat(allomasok) VALUES((SELECT allomasId FROM allomasok WHERE allomasNev = ?))", [startplace], function(err) {});
+    });
+    res.end(`Menetrend hozzáadva.`);
+});
+
+app.get('/addstop/:tourid/:stopid', function(req, res) {
+    db.run("UPDATE jaratadat SET allomasok = allomasok||','||? WHERE jaratId=?", [req.params.stopid, req.params.tourid], function(err) {
+        if(err)
+            return console.log(err);
+    });
+    res.end(`Allomas hozzaadva: ${req.params.stopid}`);
+});
+app.get('/removestop/:tourid/:stopid', function(req, res) {
+    db.run("UPDATE jaratadat SET allomasok = REPLACE(allomasok, ','||?, '') WHERE jaratId=?", [req.params.stopid, req.params.tourid], function(err) {
+        if(err)
+            return console.log(err);
+    });
+    res.end(`Allomas kitorolve: ${req.params.stopid}`);
+});
+app.get('/removetrip/:tourid', function(req, res) {
+    db.run("DELETE FROM menetrend WHERE jaratId=?", [req.params.tourid], function(err) {
+        if(err)
+            return console.log(err);
+    });
+    db.run("DELETE FROM jaratadat WHERE jaratId=?", [req.params.tourid], function(err) {
+        if(err)
+            return console.log(err);
+    });
+    res.end(`Jarat kitorolve: ${req.params.stopid}`);
+});
